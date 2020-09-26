3b2b7d4ba623e013e1ab3d861ed52726957e33dd
yongmingyang <yongmingyang1998@gmail.com>
2020-09-26 12:12:11 +0800

    checkstyleTest

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -30,6 +30,9 @@ public class PersonBuilder {
     private Remark remark;
     private Set<Tag> tags;
 
+    /**
+     * Initializes the PersonBuilder with default data
+     */
     public PersonBuilder() {
         name = new Name(DEFAULT_NAME);
         phone = new Phone(DEFAULT_PHONE);
