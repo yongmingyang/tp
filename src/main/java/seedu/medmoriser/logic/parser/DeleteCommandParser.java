10c657ebcd1462431097d060e1a2ff266bfd80f3
yongmingyang <yongmingyang1998@gmail.com>
2020-11-04 20:13:48 +0800

    fix delete error message

--- src/main/java/seedu/medmoriser/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/medmoriser/logic/parser/DeleteCommandParser.java
@@ -1,9 +1,11 @@
 package seedu.medmoriser.logic.parser;
 
 import static seedu.medmoriser.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.medmoriser.commons.core.Messages.MESSAGE_INVALID_QANDA_DISPLAYED_INDEX;
 
 import seedu.medmoriser.commons.core.index.Index;
 import seedu.medmoriser.logic.commands.DeleteCommand;
+import seedu.medmoriser.logic.commands.exceptions.CommandException;
 import seedu.medmoriser.logic.parser.exceptions.ParseException;
 
 /**
@@ -21,6 +23,9 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
             Index index = ParserUtil.parseIndex(args);
             return new DeleteCommand(index);
         } catch (ParseException pe) {
+            if (pe.getMessage().equals(MESSAGE_INVALID_QANDA_DISPLAYED_INDEX)){
+                throw new ParseException(MESSAGE_INVALID_QANDA_DISPLAYED_INDEX);
+            }
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
         }
