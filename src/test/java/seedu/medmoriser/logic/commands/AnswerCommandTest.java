2b499a16ab7a24ff65e0535d973b52588be4787a
Teng Jian Ling <tengjianling98@gmail.com>
2020-11-07 19:30:05 +0800

    Add AnswerCommandTest and comment out execute_answerOngoingQuizHasBeenAnswered_failure()

--- src/test/java/seedu/medmoriser/logic/commands/AnswerCommandTest.java
+++ src/test/java/seedu/medmoriser/logic/commands/AnswerCommandTest.java
@@ -1,42 +1,42 @@
-//package seedu.medmoriser.logic.commands;
-//
-//import static seedu.medmoriser.logic.commands.AnswerCommand.MESSAGE_ALREADY_ANSWERED;
-//import static seedu.medmoriser.logic.commands.AnswerCommand.MESSAGE_NOT_QUIZ;
-//import static seedu.medmoriser.logic.commands.AnswerCommand.MESSAGE_USER_ANSWER;
-//import static seedu.medmoriser.logic.commands.AnswerCommand.setCurrCommandResult;
-//import static seedu.medmoriser.logic.commands.CommandTestUtil.assertCommandFailure;
-//import static seedu.medmoriser.logic.commands.CommandTestUtil.assertCommandSuccess;
-//import static seedu.medmoriser.testutil.TypicalQAndA.getTypicalMedmoriser;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import seedu.medmoriser.model.Model;
-//import seedu.medmoriser.model.ModelManager;
-//import seedu.medmoriser.model.UserPrefs;
-//import seedu.medmoriser.model.qanda.QAndA;
-//import seedu.medmoriser.testutil.TypicalQAndA;
-//
-//public class AnswerCommandTest {
-//
-//    public static final String USER_ANSWER_1 = "Some answer";
-//    public static final String USER_ANSWER_2 = "Another answer";
-//
-//    private Model model = new ModelManager(getTypicalMedmoriser(), new UserPrefs());
-//
-//    @Test
-//    public void execute_answerOngoingQuizNotBeenAnswered_success() {
-//        QuizCommand.setIsQuiz(true, model);
-//        AnswerCommand.setBeenAnswered(false, model);
-//        QAndA qAndA = TypicalQAndA.QUESTION1;
-//        model.updateFilteredQAndAList(x -> x.equals(qAndA));
-//
-//        // because model not supposed to change after answer command execution
-//        Model expectedModel = model;
-//
-//        AnswerCommand answerCommand = new AnswerCommand(USER_ANSWER_1);
-//        assertCommandSuccess(answerCommand, model, MESSAGE_USER_ANSWER + USER_ANSWER_1, expectedModel);
-//    }
-//
+package seedu.medmoriser.logic.commands;
+
+import static seedu.medmoriser.logic.commands.AnswerCommand.MESSAGE_ALREADY_ANSWERED;
+import static seedu.medmoriser.logic.commands.AnswerCommand.MESSAGE_NOT_QUIZ;
+import static seedu.medmoriser.logic.commands.AnswerCommand.MESSAGE_USER_ANSWER;
+import static seedu.medmoriser.logic.commands.AnswerCommand.setCurrCommandResult;
+import static seedu.medmoriser.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.medmoriser.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.medmoriser.testutil.TypicalQAndA.getTypicalMedmoriser;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.medmoriser.model.Model;
+import seedu.medmoriser.model.ModelManager;
+import seedu.medmoriser.model.UserPrefs;
+import seedu.medmoriser.model.qanda.QAndA;
+import seedu.medmoriser.testutil.TypicalQAndA;
+
+public class AnswerCommandTest {
+
+    public static final String USER_ANSWER_1 = "Some answer";
+    public static final String USER_ANSWER_2 = "Another answer";
+
+    private Model model = new ModelManager(getTypicalMedmoriser(), new UserPrefs());
+
+    @Test
+    public void execute_answerOngoingQuizNotBeenAnswered_success() {
+        QuizCommand.setIsQuiz(true, model);
+        AnswerCommand.setBeenAnswered(false, model);
+        QAndA qAndA = TypicalQAndA.QUESTION1;
+        model.updateFilteredQAndAList(x -> x.equals(qAndA));
+
+        // because model not supposed to change after answer command execution
+        Model expectedModel = model;
+
+        AnswerCommand answerCommand = new AnswerCommand(USER_ANSWER_1);
+        assertCommandSuccess(answerCommand, model, MESSAGE_USER_ANSWER + USER_ANSWER_1, expectedModel);
+    }
+
 //    @Test
 //    public void execute_answerOngoingQuizHasBeenAnswered_failure() {
 //        QuizCommand.setIsQuiz(true, model);
@@ -49,15 +49,15 @@
 //        String repeatedAnswerMessage = USER_ANSWER_2 + "\n" + MESSAGE_ALREADY_ANSWERED;
 //        assertCommandFailure(answerCommand, model, repeatedAnswerMessage);
 //    }
-//
-//    @Test
-//    public void execute_answerNoOngoingQuiz_failure() {
-//        QuizCommand.setIsQuiz(false, model);
-//
-//        // because model not supposed to change after answer command execution
-//        Model expectedModel = model;
-//
-//        AnswerCommand answerCommand = new AnswerCommand(USER_ANSWER_1);
-//        assertCommandFailure(answerCommand, model, MESSAGE_NOT_QUIZ);
-//    }
-//}
+
+    @Test
+    public void execute_answerNoOngoingQuiz_failure() {
+        QuizCommand.setIsQuiz(false, model);
+
+        // because model not supposed to change after answer command execution
+        Model expectedModel = model;
+
+        AnswerCommand answerCommand = new AnswerCommand(USER_ANSWER_1);
+        assertCommandFailure(answerCommand, model, MESSAGE_NOT_QUIZ);
+    }
+}
